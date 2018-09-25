759797ae9c762c9b0ef7f6db8b695ba6e6a7cb7b
mactov <ch.leborgne@gmail.com>
2018-09-25 18:01:19 +0200

    added postgresql.py first method connect that creates db if it doesn't exist

--- postgresql.py
+++ postgresql.py
@@ -1,5 +1,28 @@
 import psycopg2
 from config import config
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+def connect(dbname):
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute("SELECT COUNT(*) = 0 FROM pg_catalog.pg_database"
+                    " WHERE datname = '{}'".format(dbname))
+        not_exists_row = cur.fetchone()
+        not_exists = not_exists_row[0]
+        if not_exists:
+            conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+            cur.execute('CREATE DATABASE {}'.format(dbname))
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
 
 
-
+if __name__ == '__main__':
+    connect('test')
